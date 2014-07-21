cb574c4c99e1a2141768c5462cd125bbc4b92b54
Joakin <joaquin@chimeces.com>
2014-07-21 14:20:21 +0200

    Better build and markdown in jade

---
+++ bin/build-html.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var jade = require('jade');
+var yaml = require('js-yaml');
+var md = require('marked');
+
+var extend = require('lodash.assign');
+
+var root = __dirname + '/../';
+var htmlPath = 'static/resume.html';
+var data = yaml.safeLoad(fs.readFileSync(root + 'resume.yaml', 'utf8'));
+var templateContents = fs.readFileSync(root + 'resume.jade', 'utf8');
+
+var template = jade.compile(templateContents, {
+  filename: './',
+  pretty: true,
+  compileDebug: true
+});
+
+var locals = extend(data, {
+  md: md
+});
+fs.writeFileSync(root + htmlPath, template(locals));
+
+console.log('Written ' + htmlPath);
+

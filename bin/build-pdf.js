f5378e9cdaa010ecc82674b9c0d5ecc3d3239c67
Joaquin Oltra <joaquin@chimeces.com>
2014-07-29 21:37:38 +0200

    PDF build and file

--- bin/build-pdf.js
+++ bin/build-pdf.js
@@ -6,10 +6,24 @@ var options = {
   'viewportSize': {
     'width': 1440,
     'height': 900
-  }
+  },
+  'paperSize': {
+    'pageFormat': 'A4',
+    'margin': {
+      'top': '0.7cm',
+      'left': '0.7cm',
+      'right': '0.7cm',
+      'bottom': '0.7cm'
+    },
+    'footer': {
+      'height': '1cm',
+      'contents': '{currentPage} / {pages}'
+    }
+  },
+  'zoomFactor': 0.75
 };
 
-NodePDF.render('http://www.google.com', './static/google.pdf', options, function(err, filePath) {
+NodePDF.render('./static/resume.html', './static/resume.pdf', options, function(err, filePath) {
   if (err) { console.error(err); return; }
   console.log('PDF file written at', filePath);
 });

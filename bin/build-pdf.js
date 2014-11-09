5107baf0d438ade734e5e628509d6c3a0c1e3fdc
joakin <joaquin@chimeces.com>
2014-11-09 15:00:59 +0100

    Improve pdf building script

--- bin/build-pdf.js
+++ bin/build-pdf.js
@@ -3,28 +3,35 @@
 var NodePDF = require('nodepdf');
 
 var options = {
-  'viewportSize': {
-    'width': 1440,
-    'height': 900
+  viewportSize: {
+    width: 1440,
+    height: 900
   },
-  'paperSize': {
-    'pageFormat': 'A4',
-    'margin': {
-      'top': '0.7cm',
-      'left': '0.7cm',
-      'right': '0.7cm',
-      'bottom': '0.7cm'
+  paperSize: {
+    pageFormat: 'A4',
+    margin: {
+      top: '0.7cm',
+      left: '0.7cm',
+      right: '0.7cm',
+      bottom: '0.7cm'
     },
-    'footer': {
-      'height': '1cm',
-      'contents': '{currentPage} / {pages}'
+    footer: {
+      height: '1cm',
+      contents: '{currentPage} / {pages}'
     }
   },
-  'zoomFactor': 0.75
+  zoomFactor: 0.75
 };
 
-NodePDF.render('./static/index.html', './static/resume.pdf', options, function(err, filePath) {
-  if (err) { console.error(err); return; }
-  console.log('PDF file written at', filePath);
-});
+var pdf = new NodePDF('./static/index.html', './static/resume.pdf', options);
+
+pdf.on('error', console.error);
+
+pdf.on('done', console.log.bind(console, 'PDF file written at'));
+
+pdf.on('stdout', console.log.bind(console, 'PhantomJS: '));
+
+// listen for stderr from phantomjs
+pdf.on('stderr', console.error.bind(console, 'PhantomJS: '));
+
 
